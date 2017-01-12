1d0ce56093eae694f6d1767c7d993b99fc795eab
Biddrup Kumar Mallick <biddrup3@gmail.com>
2017-01-12 11:15:15 +0600

    sure

--- 2017/01-12-2017/js/custom.js
+++ 2017/01-12-2017/js/custom.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-	$(".owl-carousel").owlCarousel();
+	$(".owl-carousel").owlCarousel({
+		items:4,
+	    lazyLoad:true,
+	    loop:true,
+	    margin:10
+	});
+	$('.next-slide .owl-carousel').owlCarousel({
+	    items:4,
+	    lazyLoad:true,
+	    loop:true,
+	    margin:10
+	});
 });
---
+++ 2017/01-12-2017/js/new.js
@@ -0,0 +1,23 @@
+var a = 5;
+var b = 20;
+var c = a + b;
+var d = a - b;
+var e = a / b;
+var f = a * b;
+
+document.write(typeof f + "<br>");
+document.write(c + "<br>");
+document.write(d + "<br>");
+document.write(e + "<br>");
+document.write(f + "<br>");
+document.write(f + "<br>");
+
+if (a == 10) {
+	document.write("I am in good shape")
+}
+else if (a == 5) {
+	document.write("I am ok")
+}
+else {
+	document.write("Let's make it good.")
+}
